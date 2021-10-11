b34337a4a1527e0d26040f2a6cea899286e3d3eb
graemekoivu <49376771+graemekoivu@users.noreply.github.com>
2021-10-11 10:01:31 -0300

    Add files via upload

---
+++ api/routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/users');
+const checkAuth = require('../middleware/check-auth');
+
+router.get('/', checkAuth, usersController.users_get_all);
+
+router.post('/signup', usersController.users_signup);
+
+router.get('/activate_account', usersController.users_activate_account);
+
+router.get('/:userId', checkAuth, usersController.users_get_user);
+
+router.patch('/:userId', checkAuth, usersController.users_patch_user);
+
+router.post('/login', usersController.users_login);
+
+router.delete('/:userId', checkAuth, usersController.users_delete_user);
+
+module.exports = router;
